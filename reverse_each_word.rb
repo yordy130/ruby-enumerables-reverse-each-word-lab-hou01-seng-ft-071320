e562593a9b43b952a483fc1f01a3036597b7ff88
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-05 00:11:45 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,7 @@ require 'pry'
 
 def reverse_each_word(sentence)
   #use .each to reverse the words in the string provided
-  sentence.split.collect {|string| string.reverse}.join(" ")
+  sentence.split.collect do |string|
+    string.reverse.join(" ")
+  end
 end
