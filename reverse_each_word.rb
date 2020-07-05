5da19e58d656e0f98cc987b2cd601e1dc2043c10
yordin rodriguez <yordinrodriguez@rocketmail.com>
2020-07-05 00:05:32 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,7 @@
+require 'pry'
+#method called reverse_each_word
+
+def reverse_each_word(sentence)
+  #use .each to reverse the words in the string provided
+  sentence.split.collect {|string| string.reverse}.join(" ")
+end
